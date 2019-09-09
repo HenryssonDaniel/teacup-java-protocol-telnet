b7bc5d3cd4e5596b11213315859bf46fec2eafae
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-09-09 20:25:34 +0200

    Add logs to Simple
    Added debug logs to Simple class.

--- src/main/java/io/github/henryssondaniel/teacup/protocol/telnet/client/Simple.java
+++ src/main/java/io/github/henryssondaniel/teacup/protocol/telnet/client/Simple.java
@@ -1,15 +1,19 @@
 package io.github.henryssondaniel.teacup.protocol.telnet.client;
 
+import io.github.henryssondaniel.teacup.core.logging.Factory;
 import io.github.henryssondaniel.teacup.protocol.telnet.Client;
 import java.io.IOException;
 import java.net.Proxy;
 import java.net.SocketException;
 import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.net.ServerSocketFactory;
 import javax.net.SocketFactory;
 import org.apache.commons.net.telnet.TelnetClient;
 
 class Simple implements Client {
+  private static final Logger LOGGER = Factory.getLogger(Simple.class);
   private final TelnetClient telnetClient;
 
   Simple(TelnetClient telnetClient) {
@@ -18,81 +22,97 @@ class Simple implements Client {
 
   @Override
   public void send(int command) throws IOException {
+    LOGGER.log(Level.FINE, "Send");
     telnetClient.sendCommand((byte) command);
   }
 
   @Override
   public void sendSubNegotiation(int... message) throws IOException {
+    LOGGER.log(Level.FINE, "Send sub negotiation");
     telnetClient.sendSubnegotiation(message);
   }
 
   @Override
   public void setCharset(Charset charset) {
+    LOGGER.log(Level.FINE, "Set charset");
     telnetClient.setCharset(charset);
   }
 
   @Override
   public void setConnectTimeout(int connectTimeout) {
+    LOGGER.log(Level.FINE, "Set connect timeout");
     telnetClient.setConnectTimeout(connectTimeout);
   }
 
   @Override
   public void setDefaultPort(int defaultPort) {
+    LOGGER.log(Level.FINE, "Set default port");
     telnetClient.setDefaultPort(defaultPort);
   }
 
   @Override
   public void setDefaultTimeout(int defaultTimeout) {
+    LOGGER.log(Level.FINE, "Set default timeout");
     telnetClient.setDefaultTimeout(defaultTimeout);
   }
 
   @Override
   public void setKeepAlive(boolean keepAlive) throws SocketException {
+    LOGGER.log(Level.FINE, "Set keep alive");
     telnetClient.setKeepAlive(keepAlive);
   }
 
   @Override
   public void setProxy(Proxy proxy) {
+    LOGGER.log(Level.FINE, "Set proxy");
     telnetClient.setProxy(proxy);
   }
 
   @Override
   public void setReaderThread(boolean readerThread) {
+    LOGGER.log(Level.FINE, "Set reader thread");
     telnetClient.setReaderThread(readerThread);
   }
 
   @Override
   public void setReceiveBufferSize(int receiveBufferSize) throws SocketException {
+    LOGGER.log(Level.FINE, "Set received buffer size");
     telnetClient.setReceiveBufferSize(receiveBufferSize);
   }
 
   @Override
   public void setSendBufferSize(int sendBufferSize) throws SocketException {
+    LOGGER.log(Level.FINE, "Set send buffer size");
     telnetClient.setSendBufferSize(sendBufferSize);
   }
 
   @Override
   public void setServerSocketFactory(ServerSocketFactory serverSocketFactory) {
+    LOGGER.log(Level.FINE, "Set server socket factory");
     telnetClient.setServerSocketFactory(serverSocketFactory);
   }
 
   @Override
   public void setSoLinger(boolean on, int soLinger) throws SocketException {
+    LOGGER.log(Level.FINE, "Set so linger");
     telnetClient.setSoLinger(on, soLinger);
   }
 
   @Override
   public void setSoTimeout(int soTimeout) throws SocketException {
+    LOGGER.log(Level.FINE, "Set so timeout");
     telnetClient.setSoTimeout(soTimeout);
   }
 
   @Override
   public void setSocketFactory(SocketFactory socketFactory) {
+    LOGGER.log(Level.FINE, "Set socket factory");
     telnetClient.setSocketFactory(socketFactory);
   }
 
   @Override
   public void setTcpNoDelay(boolean tcpNoDelay) throws SocketException {
+    LOGGER.log(Level.FINE, "Set TCP no delay");
     telnetClient.setTcpNoDelay(tcpNoDelay);
   }
 }
