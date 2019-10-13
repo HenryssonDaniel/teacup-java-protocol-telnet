af21d85659aa9a6293a36e0b77c7b085a946c7b3
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-10-13 15:01:30 +0200

    Add listener integration tests

---
+++ src/intTest/java/io/github/henryssondaniel/teacup/protocol/telnet/server/ListenerTest.java
@@ -0,0 +1,34 @@
+package io.github.henryssondaniel.teacup.protocol.telnet.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ListenerTest {
+  private final Listener listener = new ListenerImpl(1, 2, 3, 4, 5);
+
+  @Test
+  void getBacklog() {
+    assertThat(listener.getBacklog()).isEqualTo(1);
+  }
+
+  @Test
+  void getHousekeepingInterval() {
+    assertThat(listener.getHousekeepingInterval()).isEqualTo(2);
+  }
+
+  @Test
+  void getMaxConnections() {
+    assertThat(listener.getMaxConnections()).isEqualTo(3);
+  }
+
+  @Test
+  void getPort() {
+    assertThat(listener.getPort()).isEqualTo(4);
+  }
+
+  @Test
+  void getTimeout() {
+    assertThat(listener.getTimeout()).isEqualTo(5);
+  }
+}
