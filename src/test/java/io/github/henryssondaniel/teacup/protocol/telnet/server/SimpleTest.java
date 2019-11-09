46f6cdc451722a641abea8370495e9a4f004a73c
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-11-09 12:28:29 +0100

    Add more tests to SimpleTest

--- src/test/java/io/github/henryssondaniel/teacup/protocol/telnet/server/SimpleTest.java
+++ src/test/java/io/github/henryssondaniel/teacup/protocol/telnet/server/SimpleTest.java
@@ -1,16 +1,12 @@
 package io.github.henryssondaniel.teacup.protocol.telnet.server;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import io.github.henryssondaniel.teacup.protocol.Server;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.function.Supplier;
 import net.wimpi.telnetd.TelnetD;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,48 +16,70 @@ import org.mockito.MockitoAnnotations;
 class SimpleTest {
   private final Context context = mock(Context.class);
   private final Handler handler = mock(Handler.class);
-  private final Object lock = new Object();
   private final Reply reply = mock(Reply.class);
-  private final Reply replyDifferent = mock(Reply.class);
   private final TelnetD telnetD = mock(TelnetD.class);
-  private final Server<Context, Request> simpleServer = new Simple(handler, telnetD);
-  private final Object verifyLock = new Object();
+  private final Simple simple = new Simple(handler, telnetD);
 
-  @Mock private Supplier<List<Request>> supplierNonExisting;
-  private boolean waitVerify = true;
-  private boolean waiting = true;
+  @Mock private io.github.henryssondaniel.teacup.protocol.server.Handler<Request> requestHandler;
 
   @BeforeEach
   void beforeEach() {
     MockitoAnnotations.initMocks(this);
-
     when(context.getReply()).thenReturn(reply);
-    when(reply.getData()).thenReturn("message".getBytes(StandardCharsets.UTF_8));
   }
 
   @Test
-  void removeSupplier() {
-    simpleServer.removeSupplier(supplierNonExisting);
-    verifyNoInteractions(supplierNonExisting);
-  }
-
-  @Test
-  void setContext() {
-    simpleServer.setContext(context);
+  void createProtocolContext() {
+    assertThat(simple.createProtocolContext(context, requestHandler)).isNotNull();
 
     verify(context).getReply();
     verifyNoMoreInteractions(context);
 
-    verify(handler).setHandler(any());
+    verify(handler).setHandler(requestHandler);
     verify(handler).setReply(reply);
     verifyNoMoreInteractions(handler);
 
-    verifyNoInteractions(reply);
+    verifyNoInteractions(requestHandler);
+    verifyNoInteractions(telnetD);
+  }
+
+  @Test
+  void getKey() {
+    assertThat(simple.getKey(context)).isEqualTo("key");
+
+    verifyNoInteractions(context);
+    verifyNoInteractions(handler);
+    verifyNoInteractions(telnetD);
+  }
+
+  @Test
+  void isEquals() {
+    when(handler.getReply()).thenReturn(reply);
+
+    assertThat(simple.isEquals(context, handler)).isTrue();
+
+    verify(context).getReply();
+    verifyNoMoreInteractions(context);
+
+    verify(handler).getReply();
+    verifyNoMoreInteractions(handler);
+
+    verifyNoInteractions(telnetD);
+  }
+
+  @Test
+  void serverCleanup() {
+    simple.serverCleanup(handler);
+
+    verify(handler).setReply(null);
+    verifyNoMoreInteractions(handler);
+
+    verifyNoInteractions(telnetD);
   }
 
   @Test
   void setUp() {
-    simpleServer.setUp();
+    simple.setUp();
 
     verifyNoInteractions(handler);
 
@@ -71,7 +89,7 @@ class SimpleTest {
 
   @Test
   void tearDown() {
-    simpleServer.tearDown();
+    simple.tearDown();
 
     verifyNoInteractions(handler);
 
